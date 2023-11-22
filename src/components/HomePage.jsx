86348f0b41ef9981c4a14f59058a23fb871f29e4
Badejo Adebola <adebolabadejo11@gmail.com>
2023-11-22 22:53:11 +0100

    [Addition] project setup

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+        <p className='text-2xl'>Adebola</p>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from './NavBar'
+import Footer from './Footer'
+import { Outlet } from 'react-router'
+
+function Layout() {
+  return (
+    <div className="text-center text-gray-800 min-h-[100dvh]">
+      <NavBar/>
+      <main className='mt-16'>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+export default Layout

7ac4643a6e7ae9be17c503c36b8c896633565238
Badejo Adebola <adebolabadejo11@gmail.com>
2023-12-30 01:06:07 +0100

    Minor additions to pricing

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -6,13 +6,13 @@ import PricingCard from './PricingCard'
 function Pricing() {
     return (
         <div>
-            <div className='py-14 md:px-28 px-5 bg-gradient-to-b from-[#B6BAFF33] to-[#232ED133] h-screen mx-auto'>
+            <div className='py-14 px-5 bg-gradient-to-b from-[#B6BAFF33] to-[#232ED133] h-screen mx-auto'>
                 <div className='text-center'>
                     <h1 className='text-black text-4xl max-lg:text-3xl font-bold'>Pricing</h1>
-                    <p className='py-10'>Lorem ipsum dolor sit amet, consectetur adipiscing<br /> elit. Vestibulum vitae nunc vitae neque</p>
+                    <p className='py-10'>Lorem ipsum dolor sit amet, consectetur adipiscing<br className='max-md:hidden' /> elit. Vestibulum vitae nunc vitae neque</p>
                 </div>
             </div >
-            <PricingCard />
+            <div className=''><PricingCard /></div>
             <Faq />
             <CustomerTestimonials />
         </div>
--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
-import { FaCheck } from "react-icons/fa6";
-
 
 function PricingCard() {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 max-lg:px-5 px-28 gap-20 place-items-center relative -top-60">
-            <div className='block text-left max-w-[500px] max-h-[550px] bg-gradient-to-b from-[#B6BAFF33] to-[#232ED133] rounded-xl p-10'>
+        <div className="grid grid-cols-1 md:grid-cols-3 max-lg:px-5 px-28 gap-20 place-items-center">
+            <div className=' text-left max-w-[500px] max-h-[550px] bg-gradient-to-b from-[#FFFFFF] to-[#ECEDFF] rounded p-10'>
                 <h1 className='font-bold text-black text-2xl my-2'>Launch</h1>
-                <p className='text-2xl font-bold my-2'>$<span className='text-5xl font-bold text-black my-4'>19</span>/mo</p>
+                <p className='text-2xl font-bold my-4'>$<span className='text-5xl font-bold text-black my-4'>19</span>/mo</p>
                 <p className='text-gray-500 font-semibold my-2'>Integer proin ac, lectus proin egestas ultricies. </p>
-                <ul className='font-semibold list-image-icon mx-7'>
+                <ul className='font-semibold list-image-icon my-8 mx-7'>
                     <li className='my-2'>1GB of storage</li>
                     <li className='my-2'>1 user</li>
                     <li className='my-2'>50 tasks</li>
                 </ul>
                 <button className='mt-20 w-full py-3 px-8 bg-transparent border rounded font-semibold text-sm border-blue-800 text-blue-800'>Get Started</button>
             </div>
-            <div className='block text-left max-w-[500px] max-h-[550px] bg-gradient-to-b from-[#fce7e333] to-[#ad3d2433] rounded-xl p-10'>
-                <h1 className='font-bold text-black text-2xl my-2'>Launch</h1>
-                <p className='text-2xl font-bold my-2'>$<span className='text-5xl font-bold text-black my-4'>49</span>/mo</p>
+            <div className='text-left max-w-[500px] max-h-[550px] bg-gradient-to-b from-[#FFFFFF] to-[#FFE8E2] rounded p-10'>
+                <h1 className='font-bold text-black text-2xl my-2'>Startup</h1>
+                <p className='text-2xl font-bold my-4'>$<span className='text-5xl font-bold text-black my-4'>49</span>/mo</p>
                 <p className='text-gray-500 font-semibold my-2'>Integer proin ac, lectus proin egestas ultricies. </p>
-                <ul className='font-semibold list-image-icons mx-7'>
+                <ul className='font-semibold list-image-icons my-8 mx-7'>
                     <li className='my-2'>10GB of storage</li>
                     <li className='my-2'>3 user</li>
                     <li className='my-2'>500 tasks</li>
@@ -28,11 +26,11 @@ function PricingCard() {
                 </ul>
                 <button className='mt-14 w-full py-3 px-8 bg-[#F24C27] border rounded font-semibold text-sm border-[#F24C27] text-white'>Get Started</button>
             </div>
-            <div className='block text-left max-w-[500px] max-h-[550px] bg-gradient-to-b from-[#c0c3ff33] to-[#343eca33] rounded-xl p-10'>
-                <h1 className='font-bold text-black text-2xl my-2'>Launch</h1>
-                <p className='text-2xl font-bold my-2'>$<span className='text-5xl font-bold text-black my-4'>79</span>/mo</p>
+            <div className='text-left max-w-[500px] max-h-[550px] bg-gradient-to-b from-[#FFFFFF] to-[#ECEDFF] rounded p-10'>
+                <h1 className='font-bold text-black text-2xl my-2'>Growth</h1>
+                <p className='text-2xl font-bold my-4'>$<span className='text-5xl font-bold text-black my-4'>79</span>/mo</p>
                 <p className='text-gray-500 font-semibold my-2'>Integer proin ac, lectus proin egestas ultricies. </p>
-                <ul className='font-semibold list-image-icon mx-7'>
+                <ul className='font-semibold list-image-icon my-8 mx-7'>
                     <li className='my-2'>100GB of storage</li>
                     <li className='my-2'>Unlimited user</li>
                     <li className='my-2'>Unlimited tasks</li>
